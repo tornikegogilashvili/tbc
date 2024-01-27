cf9712e34e1462b81969dbb16dfe0b3590cab61e
tornike gogilashvili <tornikegogilashvili@yahoo.com>
2024-01-27 16:06:46 +0400

    navbar changes

--- src/App.js
+++ src/App.js
@@ -1,60 +1,35 @@
 import './App.css';
-
+import tbcicon from  './img/icon.svg'
 function App() {
   return (
     <div>
-      {/* <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button
-      data-mdb-collapse-init
-      class="navbar-toggler"
-      type="button"
-      data-mdb-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="true"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        
-      </ul>
-    </div>
-  </div>
-      </nav> */}
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+      <div class="container-fluid imagediv">
+        <div className='leftdiv'>
+          <a href='#'><img src={tbcicon} alt='tbcicon' className='tbcicon' /></a>
+    
+        <a class="navbar-brand" href="#">&nbsp;&nbsp;  აკადემია</a>
+        </div>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">მთავარი</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TBC IT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TBC x USAID</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">რისკები</a>
+          </li>
+        </ul>
+      </div>
   </div>
 </nav>
     </div>
